eebb5a2bbcbc13faf68d366ba6bfa314e70a0698
pablomendoza76 <116389858+pablomendoza76@users.noreply.github.com>
2023-01-03 15:11:13 -0500

    Add files via upload

---
+++ src/Ordenamiento/Seleccion.java
@@ -0,0 +1,31 @@
+package Ordenamiento;
+
+public class Seleccion {
+    public static void main(String[] args) throws InterruptedException{
+        int arreglo[]=new int[1000];
+        int indiceelem=0;
+        for (int indice = 1000; indice > 0; indice--) {
+            arreglo[indiceelem]=indice;
+            indiceelem++;
+        }
+        long inicio= System.currentTimeMillis();
+        Thread.sleep(2000);
+        for (int indice = 0; indice < arreglo.length; indice++) {
+            int min=indice;
+            for (int indice2 = indice+1; indice2 < arreglo.length; indice2++) {
+                if(arreglo[indice2]<arreglo[min]){
+                    min=indice2;
+                }
+            }
+            int aux = arreglo[indice];
+            arreglo[indice]=arreglo[min];
+            arreglo[min]=aux;
+        }
+        for (int indice = 0; indice < arreglo.length; indice++) {
+            System.out.println(arreglo[indice]);
+        }
+        long fin = System.currentTimeMillis();
+        double tiempo= (double) ((fin-inicio)/1000);
+        System.out.println(tiempo+" segundos");
+    }
+}
